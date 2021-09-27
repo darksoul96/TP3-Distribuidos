a57909a5054169000cf11971cef15096faec2b06
darksoul96 <62730262+darksoul96@users.noreply.github.com>
2021-09-27 14:48:31 -0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,1 +1,24 @@
-//server
+const http = require('http');
+const port;
+
+const file = {
+    filename,
+    filesize,
+    nodeIP,
+    nodePort,
+}
+
+const server = http.createServer((request, response) => {
+
+    request.on('data', (chunk) => {
+        file = chunk;
+    });
+
+    request.on('end', () => {
+        console.log(file)
+    });
+});
+
+server.listen(port, () => {
+    console.log('Server on');
+});

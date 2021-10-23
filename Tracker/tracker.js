69a3ec3b0b429ff21ba1241502692733bab6879d
leopard3142 <39896829+leopard3142@users.noreply.github.com>
2021-10-23 19:09:31 -0300

    asd

--- Tracker/tracker.js
+++ Tracker/tracker.js
@@ -1,28 +1,44 @@
-const udp = require('dgram');
-const ht = require('./utils/hashtable');
-const trackerClient = udp.createSocket('udp4');
+const udp = require("dgram");
+const ht = require("./utils/hashtable.js");
+const trackerClient = udp.createSocket("udp4");
+
+trackerClient.bind({
+  address: "localhost",
+  port: "8399",
+  exclusive: true,
+});
 
 var hashEntry = {
-    hash: '',
-    fileName: '',
-    filesize: null,
-    nodeIP: '',
-    nodePort: null
+  hash: "",
+  fileName: "",
+  filesize: null,
+  nodeIP: "",
+  nodePort: null,
 };
 
+trackerClient.on("message", (msg, info) => {
+  console.log(
+    `Recibio el mensaje: ${msg} de la direccion: ${info.address} con puerto: ${info.port}`
+  );
 
-trackerClient.on('message', (msg, info) => {
-    console.log('Recibio el mensaje: ${msg} de la direccion: ${info.address} con puerto: ${info.port}');
-
-    loadEntry(hashEntry, msg, info);
-    let arrayInfo = { filename: hashEntry.fileName, size: hash.hashEntry.size, par: { ip: hashEntry.nodeIP, port: hashEntry.nodePort } };
-    ht.set(hash, arrayInfo);
+  loadEntry(hashEntry, msg, info);
+  let arrayInfo = {
+    filename: hashEntry.fileName,
+    size: hash.hashEntry.size,
+    par: { ip: hashEntry.nodeIP, port: hashEntry.nodePort },
+  };
+  ht.set(hash, arrayInfo);
 });
 
 function loadEntry(hashEntry, msg, info) {
-    hashEntry.hash = msg.body.hash;
-    hashEntry.fileName = msg.body.fileName;
-    hashEntry.size = msg.body.filesize;
-    hashEntry.nodeIP = info.address;
-    hashEntry.nodePort = info.port;
+  hashEntry.hash = msg.body.hash;
+  hashEntry.fileName = msg.body.fileName;
+  hashEntry.size = msg.body.filesize;
+  hashEntry.nodeIP = info.address;
+  hashEntry.nodePort = info.port;
 }
+
+trackerClient.on("listening", () => {
+  let address = trackerClient.address();
+  console.log(`Tracker escuchando en ${address.address}:${address.port}...`);
+});

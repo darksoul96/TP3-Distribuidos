80b195370cbb63ad9e28b419f6b2097deabe820d
darksoul96 <62730262+darksoul96@users.noreply.github.com>
2021-10-23 17:12:35 -0300

    hash del archivo

--- Server/src/server.js
+++ Server/src/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const crypto = require('crypto');
 
 const port = 8080;
 
@@ -14,6 +15,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.post("/", (req, res) => {
-  console.log(req.body);
+  let file = {
+    id: req.body.id,
+    filename: req.body.filename,
+    filesize: req.body.filesize,
+    nodeIP: req.body.nodeIP,
+    nodePort: req.body.nodePort,
+  };
+  let hash = crypto.createHash('sha1');
+  hash.update(file.filename + Math.round(file.filesize).toString());
+  console.log(hash.digest('hex'));
+  console.log(file.filename);
   res.status(201).send("File Recieved");
 });

44461a87def1c38552bbdbee199fab9eb10e1975
darksoul96 <62730262+darksoul96@users.noreply.github.com>
2021-12-02 18:54:30 -0300

    nodos par

---
+++ Par/par.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+const dgram = require("dgram");
+const parClient = dgram.createSocket("udp4");
+const args = process.argv;
+
+var localaddress, localport, id;
+
+var nodoDerecha = {
+    addressD: null,
+    portD: null,
+};
+
+var nodoIzquierda = {
+    addressI: null,
+    portI: null,
+};
+
+if (args.length > 2) id = args[2];
+else id = 1;
+
+const initPar = async function () {
+    var nodos = await JSON.parse(
+        fs.readFileSync("./config/nodos_par.json", "utf8")
+    )["nodos"];
+    let index = nodos.findIndex((nodo) => nodo.id == id);
+    console.log("ADDRESS: " + nodos[index].address);
+    localaddress = nodos[index].address;
+    localport = nodos[index].port;
+    nodoIzquierda = nodos[index].nodoIzquierda;
+    nodoDerecha = nodos[index].nodoDerecha;
+
+    console.log("ID: " + id);
+    parClient.bind({
+        address: localaddress,
+        port: localport,
+        excluse: true,
+    });
+};
+
+initPar();
